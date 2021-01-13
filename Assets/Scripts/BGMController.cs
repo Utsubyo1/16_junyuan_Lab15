2bb98d8c41a7b34f333b5aeb6dd9e364f771a5e1
utsubyo <72735425+ustubyo@users.noreply.github.com>
2021-01-13 16:28:20 +0800

    Initial commit

---
+++ Assets/Scripts/BGMController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMController : MonoBehaviour
+{
+
+    public AudioClip[] BGMArr;
+    private AudioSource audioSource;
+    int tabcount = 1;
+    float vol = 1f;
+    float volrate = 0.1f;
+
+    bool Stop = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        //play first BGM audio
+        audioSource.PlayOneShot(BGMArr[0]); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            //increase the number of tabcount when press tab key
+            //every increase will change to a new music
+            tabcount++;
+            //int rand = Random.Range(0, BGMArr.Length);
+            if (tabcount == 1)
+            {
+                audioSource.Stop();
+                audioSource.PlayOneShot(BGMArr[0]);
+            }
+            if (tabcount == 2)
+            {
+                audioSource.Stop();
+                audioSource.PlayOneShot(BGMArr[1]);
+            }
+            if (tabcount == 3)
+            {
+                audioSource.Stop();
+                audioSource.PlayOneShot(BGMArr[2]);
+            }
+            if(tabcount > 3)
+            {
+                audioSource.Stop();
+                audioSource.PlayOneShot(BGMArr[0]);
+                tabcount = 1;
+                
+            }
+
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            vol = vol + (volrate * Time.deltaTime);
+            audioSource.volume = vol;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            vol = vol - (volrate * Time.deltaTime);
+            audioSource.volume = vol;
+        }
+
+       
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject Jumptext;
+    public AudioClip[] AudioClipArr;
+    private AudioSource audioSource;
+    
+    private int JumpCounter;
+    private Rigidbody PlayerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpCounter++;
+            Jumptext.GetComponent<Text>().text = "Jump: " + JumpCounter;
+            PlayerRb.AddForce(new Vector3(0, 250, 0));
+
+            //audioSource.Play();
+            int rand = Random.Range(0, AudioClipArr.Length);
+            audioSource.PlayOneShot(AudioClipArr[rand]);
+        }
+        
+    }
+}
+
